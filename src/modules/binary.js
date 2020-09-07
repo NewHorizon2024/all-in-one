a17e5df1c2b0caeb083b9da3ee2b0945793bdb61
Mostafa <mustafa.husseiny@gmail.com>
2020-09-08 00:14:01 +0200

    ref

--- src/modules/binary.js
+++ src/modules/binary.js
@@ -1,9 +1,11 @@
 'use strict';
 
+
+export function checker() {
+    alert('check dynamic import system');
+}
+
 export function rock() {
     alert("modules javascript");
 }
 
-export function checker() {
-    alert('check dynamic import system')
-}
