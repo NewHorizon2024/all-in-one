58e87780d85f4595179600ba02c5c525b9a35864
Mostafa <mustafa.husseiny@gmail.com>
2020-09-10 22:32:09 +0200

    switch case

--- src/index.js
+++ src/index.js
@@ -38,6 +38,86 @@ class Btn extends HTMLElement {
         const appTitle = this.getAttribute('title');
         btnTitle.textContent = appTitle;
         btnTitle.onclick = function() {
+            //let myApp = 'my-app';
+            switch(appTitle) {
+                case 'Converting to Binary':
+                    let myApp = 'my-app';
+                    new Promise(resolve => {
+                        if (view.children.length > 0) {
+                            view.children[0].remove();
+                            return;
+                        }
+                        if (customElements.get('my-app')) {
+                            console.log("Done");
+                        } else {
+                            window.customElements.define('my-app', Binary);
+                        }
+                        resolve(Binary);
+                    })
+                    .then(classObj => {
+                        const lam = document.createElement('my-app');
+                        view.append(lam);
+                    })
+                    .catch(err => alert(err.stack))
+                default: return;
+
+
+            }
+            
+            
+        }
+
+    }
+
+
+
+
+
+
+}
+
+window.customElements.define('apps-btn', Btn);
+
+
+
+
+/*
+class Btn extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
+        const root = this.shadowRoot;
+        root.innerHTML = `
+        <div id = 'control-view-container'>
+            <div id = 'btn'></div>
+        </div>
+
+        <style>
+        #btn {
+            width: 15vw;
+            height: 40px;
+            box-shadow: 0px 0px 10px 5px #343132;
+            border-radius: 5px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #ffff;
+            font-family: 'ABeeZee';
+            font-size: 16px;
+            margin-top: 10px;
+            cursor: pointer;
+            transition: 0.5s ease-out;
+        }
+        </style>
+        `;
+    }
+   
+    connectedCallback() {
+        const view = document.getElementById('view');
+        const btnTitle = this.shadowRoot.getElementById('btn');
+        const appTitle = this.getAttribute('title');
+        btnTitle.textContent = appTitle;
+        btnTitle.onclick = function() {
             let myApp = 'my-app';
             new Promise(resolve => {
                 if (view.children.length > 0) {
@@ -65,10 +145,6 @@ class Btn extends HTMLElement {
 }
 
 window.customElements.define('apps-btn', Btn);
-
-
-
-
-
+*/
 
 
