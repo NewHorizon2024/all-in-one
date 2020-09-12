781b6d3cf215c37f35d6335ea03b693f37bf71cc
Mostafa <mustafa.husseiny@gmail.com>
2020-09-12 15:14:46 +0200

    sd

--- src/index.js
+++ src/index.js
@@ -150,9 +150,8 @@ window.customElements.define('apps-btn', Btn);
 
 
 const pointer = document.getElementsByClassName('fa-braille')[0];
+
 pointer.onclick = function() {
-    alert("D")
     const menu = document.getElementsByClassName('set')[0];
-    
     menu.classList.toggle('open');
 }
